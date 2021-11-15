38198fcb943ff875d20beb04f9edc43ef6cd5f86
ryhn7 <rayhan205732@gmail.com>
2021-11-15 09:45:56 +0700

    delete the unused color

--- core/lib/src/styles/colors.dart
+++ core/lib/src/styles/colors.dart
@@ -8,12 +8,7 @@ const Color kNetflixRed = Color(0xFFE50914);
 const Color kDavysGrey = Color(0xFF4B5358);
 const Color kGrey = Color(0xFF303030);
 const Color kMkdYellow = Color(0xFFffc300);
-// const Color kRichBlack = Color(0xFF000814);
-// const Color kOxfordBlue = Color(0xFF001D3D);
-// const Color kPrussianBlue = Color(0xFF003566);
-// const Color kNetflixRed = Color(0xFFffc300);
-// const Color kDavysGrey = Color(0xFF4B5358);
-// const Color kGrey = Color(0xFF303030);
+
 
 const kColorScheme = ColorScheme(
   primary: kNetflixRed,
