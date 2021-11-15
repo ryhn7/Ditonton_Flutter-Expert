1c32a108d01d81ac90927d5c445ab45e4655cc40
ryhn7 <rayhan205732@gmail.com>
2021-11-15 08:27:00 +0700

    Add netflix color

--- core/lib/src/styles/colors.dart
+++ core/lib/src/styles/colors.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 // colors
 const Color kRichBlack = Color(0xFF000814);
 const Color kOxfordBlue = Color(0xFF001D3D);
@@ -8,6 +7,7 @@ const Color kPrussianBlue = Color(0xFF003566);
 const Color kMikadoYellow = Color(0xFFffc300);
 const Color kDavysGrey = Color(0xFF4B5358);
 const Color kGrey = Color(0xFF303030);
+const Color kRed = Color(0xFFE50914);
 // const Color kRichBlack = Color(0xFF000814);
 // const Color kOxfordBlue = Color(0xFF001D3D);
 // const Color kPrussianBlue = Color(0xFF003566);
